1f30ac9f845bbd7e4b76da4a4c39b98aa7bc3e51
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2023-10-08 23:20:15 +0900

    section10 finish

---
+++ section10_ESmodule/src/models/project.ts
@@ -0,0 +1,16 @@
+// status type
+export enum ProjectStatus {
+  Active,
+  Finished,
+}
+
+//Project type
+export class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public manday: number,
+    public status: ProjectStatus
+  ) {}
+}
